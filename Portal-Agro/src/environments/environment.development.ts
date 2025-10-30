4ddafa282910b905241f2356d4e85a7595cb32c1
Sergio <161945446+Leguizamo2502@users.noreply.github.com>
2025-10-30 02:51:02 -0500

    Update apiUrl to use port 5100

--- Portal-Agro/src/environments/environment.development.ts
+++ Portal-Agro/src/environments/environment.development.ts
@@ -2,7 +2,7 @@ export const environment = {
     // apiUrl: 'https://localhost:7050/api/v1/'
     // apiUrl: 'https://localhost/api/v1/'
     // apiUrl: 'http://localhost:8080/api/v1/'
-    apiUrl:'http://localhost:5103/api/v1/'
+    apiUrl:'http://localhost:5100/api/v1/'
 
 
 };
